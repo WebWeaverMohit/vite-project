5a73c64105adc73015c4ed5e7f52d99c3828bb89
WebWeaverMohit <mohitshrivastava0000@gmail.com>
2024-04-03 13:30:52 +0530

    learning react.js

--- src/App.jsx
+++ src/App.jsx
@@ -35,15 +35,22 @@ import { useState } from "react"
 // }
 
 const App = () => {
-  // showinfo = () => {
-    
-  // }
+
+  const [username, setusername] = useState("")
+  const [email, setemail] = useState("")
+  const [password, setpassword] = useState("")
+
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log({username, email, password})
+  }
   return (
     <>
-      <form action="">
-        <input type="text" placeholder="username" name="username" />
-        <input type="email" placeholder="email" name="email" />
-        <input type="password" placeholder="password" name="password" />
+      <form onSubmit={submitHandler}>
+        <input onChange={(e)=> setusername(e.target.value)} type="text" placeholder="username" value={username} name="username" /><br /><br />
+        <input onChange={(e)=> setemail(e.target.value)} type="email" placeholder="email" value={email} name="email" /><br /><br />
+        <input onChange={(e)=> setpassword(e.target.value)} type="password" placeholder="password" value={password} name="password" /><br /><br />
         <input type="submit" value="submit" />
       </form>
     </>
